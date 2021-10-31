2c6f69979865482ab8f55f14cfd7c49b3609c1c1
kaiser-mousu <72510555+kaiser-mousu@users.noreply.github.com>
2021-10-31 12:31:07 +0100

    Nettoyé le code, ajouté lisibilité et commentaires
    Ajoute plus de proprété au script

--- edt3il.py
+++ edt3il.py
@@ -1,135 +1,162 @@
+######################################################################
+##                  NECESSARY DEPENDANCIES                          ##
+##                  OR ELSE IT WONT                                 ##
+##                  WORK                                            ##
+##                  @kaiser-mousu IN GITHUB                         ##
+######################################################################
 from bs4 import BeautifulSoup
 import numpy as np
 import re as re
 from datetime import datetime
 import csv
-
-
-# import math as m
+import math as m
 import requests
-#Recuperation de la page web
-url = "http://eleves.groupe3il.fr/edt_eleves/00_index.php?idGroupe=I2+Groupe+1.xml"
+import exportICS
+
+#Création du lien en fonction du groupe I2
+lienBase = "https://eleves.groupe3il.fr/edt_eleves/00_index.php?idGroupe="
+groupe=input("C'est quoi ton groupe (1, 2, 3, 4, 5+FA, 6+FA)?")
+url=lienBase+"I2+Groupe+"+groupe+".xml"
+
+#Récuperation du lien et parsing html
 req = requests.get(url)
 soup = BeautifulSoup(req.text, "html.parser")
-trait = soup.body
+traitementBase = soup.body
 #On traite les infos, on prends tous les strings (stripped ou traités) et on les mets dans un array
-var = []
-for string in trait.stripped_strings:
+listeNonParsee = []
+for string in traitementBase.stripped_strings:
     if(string=="Horaire"):
-        var.append("Eliminer")
+        listeNonParsee.append("Eliminer")
     else:
-        var.append(string)
-#On fait un 2eme traitement, et on élimine toutes les données inutiles qui appartienent au navbar ou autre
-#On coupe en semaine, 1 
-num=np.isin(var,"Eliminer")
-lanum=[]
-for i in range(len(num)):
-    if num[i]==True:
-        lanum.append(i)
-lanum.append(len(var))
-      
-neovar=[]
-for i in range(len(lanum)-1):
-    neovar.append(var[lanum[i]+7:lanum[i+1]-1])
-
-
-bt=-1
-count=-1
-letr=np.zeros((200,6),dtype=object)
-jours=["Lundi","Mardi","Mercredi","Jeudi","Vendredi"]
-for e in neovar:
-    for i in range(len(e)):
-        if e[i] in jours:
-            bt+=1
-            count=-1
-        print(bt,count)
-        print(e[i])
-        count+=1
-        letr[bt][count]=e[i]
-
-for i in range(len(letr)):
-    if letr[i][5]!=0:
-        if "h" in letr[i][5]:
-            letr[i][4]=letr[i][5]
-            letr[i][5]=0
-    if letr[i][3]!=0:     
-        if "h" in letr[i][3]:
-            letr[i][4]=letr[i][3]
-            letr[i][3]=0
-    if letr[i][2]!=0:
-        if "h" in letr[i][2]:
-            letr[i][4]=letr[i][2]
-            letr[i][2]=0
-
+        listeNonParsee.append(string)
         
-for i in range(len(letr)-1):
-    letr[i][2]=letr[i+1][2]
-    letr[i+1][2]=0
-    
-    letr[i][3]=letr[i+1][3]
-    letr[i+1][3]=0
-    
-    
-aeli=[] #liste de trucs a eliminer
-for i in range(len(letr)):
-    if (letr[i][2]==0) and (letr[i-1][2]==0) and (letr[i][3]==0) and (letr[i][4]!=0):
-        letr[i-1][4]=letr[i][4]
-        letr[i][4]=0
+#On fait un 2eme traitement, et on élimine toutes les données inutiles qui appartienent au navbar ou autre
+#On coupe par semaine
+#D'abord je cree une matrice booleene qui met true aux positions ou il faut trancher par semaine
+matriceBoolene=np.isin(listeNonParsee,"Eliminer")
+positions=[]
+for i in range(len(matriceBoolene)):
+    if matriceBoolene[i]==True:
+        positions.append(i)
+positions.append(len(matriceBoolene))
+#Puis on met sur chaque ligne une semaine, remplie des matieres (avec les differentes données)
+semaines=[]
+for i in range(len(positions)-1):
+    semaines.append(listeNonParsee[positions[i]+7:positions[i+1]-1])
 
-for i in range(len(letr)):
-    if letr[i][2]==0:
-        aeli.append(i)
+#Cette partie est nottament reserve pour le traitement et mise en case des diffeents elements
+# D'abord je crée les differents compteurs position et la matrice pure, composée d'elements vides de taille semaines*5(les jours)*6(matieres qu'on avec un surplus au cas ou)
+positionI=-1
+positionJ=-1
+edtPur=np.zeros((m.floor(len(positions)*5*6),6),dtype=object)
+jours=["Lundi","Mardi","Mercredi","Jeudi","Vendredi"] #J'ai remarqué que dans la structure ca commence toujours par le jour donc je l'utilise comme delimiteur de chaque ligne
+for semaine in semaines:
+    for i in range(len(semaine)):
+        if semaine[i] in jours: 
+            positionI+=1
+            positionJ=-1 #Si c'est un des jours, on avance de ligne et on remet la position a la base
+        positionJ+=1
+        edtPur[positionI][positionJ]=semaine[i]
 
-nletr = np.delete(letr,aeli,axis=0) #tu ne peux l'appliquer qu'une fois, ça marche pas dans une boucle
+#Des fois les horaires sont dans des colonnes differentes car ils ont pas mis un standard pour communiquer les infos (merci pour le coup de me donner plus de taf)
+#donc je cherche s'il y a un h contenu dans la case des salles (comme 10h30) et je le met a sa bonne case
+for i in range(len(edtPur)):
+    if edtPur[i][5]!=0:
+        if "h" in edtPur[i][5]:
+            edtPur[i][4]=edtPur[i][5]
+            edtPur[i][5]=0
+    if edtPur[i][3]!=0:     
+        if "h" in edtPur[i][3]:
+            edtPur[i][4]=edtPur[i][3]
+            edtPur[i][3]=0
+    if edtPur[i][2]!=0:
+        if "h" in edtPur[i][2]:
+            edtPur[i][4]=edtPur[i][2]
+            edtPur[i][2]=0
 
-for i in range(len(nletr)):
-    nletr[i][4]=nletr[i][4].replace('h',':')
-            
+#Je ne sais pourquoi, mais il y a un decalage dans les salles et horaires par rapport aux sujets, donc je decale vers le haut de 1
+for i in range(len(edtPur)-1):
+    edtPur[i][2]=edtPur[i+1][2]
+    edtPur[i+1][2]=0
+    
+    edtPur[i][3]=edtPur[i+1][3]
+    edtPur[i+1][3]=0
+    
+#Il y a bcp de lignes vides,et des informations sur 2 lignes (horaire sur une ligne, matiere et salle dans celle d'apres), donc je cherche a les fusionner avec un tes
+# si la matiere de cette ligne et celle d'avant est vide mais qu'il y a un horaire est une salle dans celle d'avant je fusionne
+# en regle generale c'est les EPP ou autres conferences
+positionsEliminer=[] #liste de trucs a eliminer
+for i in range(len(edtPur)):
+    if (edtPur[i][2]==0) and (edtPur[i-1][2]==0) and (edtPur[i][3]==0) and (edtPur[i][4]!=0):
+        edtPur[i-1][4]=edtPur[i][4]
+        edtPur[i][4]=0
+#On prends les lignes vides (sans matiere) et on les elimine
+for i in range(len(edtPur)):
+    if edtPur[i][2]==0:
+        positionsEliminer.append(i)
+edtTraite = np.delete(edtPur,positionsEliminer,axis=0) #tu ne peux l'appliquer qu'une fois, ça marche pas dans une boucle
+
+#CSV et ICS sont des standards americains, donc ils utilisent : a la place de h, donc on remplace
+for i in range(len(edtTraite)):
+    edtTraite[i][4]=edtTraite[i][4].replace('h',':')
+
+#Des fois les heures sont EPP donc il y a plus d'info que l'heure donc il faut decouper l'heure de la matiere
 temp = []
 mot= ""
-for i in range(len(nletr)):
-    if "2021" not in nletr[i][1]:
-        nletr[i][1]=nletr[i][1]+"/2021"
-        
-    temp=re.split("[ |-]+", nletr[i][4]) 
-    if len(temp)>3:
+for i in range(len(edtTraite)):
+    if "2021" not in edtTraite[i][1]:
+        edtTraite[i][1]=edtTraite[i][1]+"/2021" #On ajoute l'année la ou elle est pas
+    temp=re.split("[ |-]+", edtTraite[i][4]) #On split utilisant REGEX
+    if len(temp)>2: 
         for j in range(len(temp)-2):
             mot+=temp[j+2]+" "
         temp=temp[:2]
-        nletr[i][2]=mot
-        nletr[i][4]=temp[0]
-        nletr[i][5]=temp[1]
+        edtTraite[i][2]=mot
+        edtTraite[i][4]=temp[0]
+        edtTraite[i][5]=temp[1] #Si le mot est plus grand que 2, il y a que les indices 0 et 1 qui contiennent l'heure, le reste c'est des mots donc matiere
         temp=[]
-        mot=''        
+        mot=''                  #On les remet a 0 
     elif len(temp)<2:
-        nletr[i][4]=temp[0]
-        nletr[i][5]="19h00"
+        edtTraite[i][4]=temp[0]
+        edtTraite[i][5]=None    #S'il y a pas d'horaire de fin google ajoute automatiquement 1h donc EPP ou autre
     else:
-        nletr[i][4],nletr[i][5]=temp
+        edtTraite[i][4],edtTraite[i][5]=temp
+#Traitement des heures et dates pour standardiser vers ICS et CSV
 d=""
 h=""
-for i in range(len(nletr)):
-    d=datetime.strptime(nletr[i][1], "%d/%m/%Y")
-    nletr[i][1]=d.strftime("%d/%m/%Y")
-    h=datetime.strptime(nletr[i][4], "%H:%M")
-    nletr[i][4]=h.strftime("%I:%M %p")
-    h=datetime.strptime(nletr[i][5], "%H:%M")
-    nletr[i][5]=h.strftime("%I:%M %p")
+for i in range(len(edtTraite)):
+    d=datetime.strptime(edtTraite[i][1], "%d/%m/%Y")
+    edtTraite[i][1]=d.strftime("%d/%m/%Y")
+    try:
+        h=datetime.strptime(edtTraite[i][4], "%H:%M")
+        edtTraite[i][4]=h.strftime("%I:%M %p")
+    except:
+        edtTraite[i][4]=None
+    try:
+        h=datetime.strptime(edtTraite[i][5], "%H:%M")
+        edtTraite[i][5]=h.strftime("%I:%M %p")
+    except:
+        edtTraite[i][5]=None
     h=""
-    nletr[i][0]="FALSE"
+    edtTraite[i][0]="FALSE"         #Il faut une colonne FALSE pour ALL-DAY-EVENT donc on transforme
+    edtTraite[i][2]=edtTraite[i][2]+" en "+str(edtTraite[i][3]) #On ajoute la salle a la matiere pour plus de visibilité
     
-titre=['Subject','Start Date','Start Time','End Date','End Time','All Day Event','Description','Location']
-nletr=nletr.transpose()
-nletr=np.vstack([nletr,np.empty((1,len(nletr[0])),dtype=object)])
-nletr=np.vstack([nletr,np.empty((1,len(nletr[0])),dtype=object)])
+    
+    
+titre=['Subject','Start Date','Start Time','End Date','End Time','All Day Event','Description','Location'] #Premiere colonne du CSV
+#La on transpose, on bouge les lignes dans le bon ordre, on ajoute certaines colonnes vides et on reconvertis en colonnes
+edtTraite=edtTraite.transpose()
+edtTraite=np.vstack([edtTraite,np.empty((1,len(edtTraite[0])),dtype=object)])
+edtTraite=np.vstack([edtTraite,np.empty((1,len(edtTraite[0])),dtype=object)])
+edtTraite[[0,2,4]]=edtTraite[[2,4,0]]
+edtTraite[[3,4,5,6]]=edtTraite[[6,5,4,3]]
+edtTraite=edtTraite.transpose()
 
-nletr[[0,2,4]]=nletr[[2,4,0]]
-nletr[[3,4,5,6]]=nletr[[6,5,4,3]]
-
-nletr=nletr.transpose()
-
+#Ecriture du CSV
 with open('export.csv', 'w', encoding='UTF8', newline='') as f:
     writer=csv.writer(f)
     writer.writerow(titre)
-    for elm in nletr:
-        writer.writerow(elm)
+    for element in edtTraite:
+        writer.writerow(element)
+
+exportICS.importerICS() #Execute le script CSV>ICS
